488dfac15f994bd42b19c0d93b8fadd8b98c1d62
jihoon0330 <jihoon@hoztec.com>
2022-03-06 21:08:23 +0900

    data 값 추가

---
+++ data/twitter/dataMedia.ts
@@ -0,0 +1,35 @@
+import { Twitter } from "../../types/twitter/twitter";
+
+const dataMedia = {
+  sortIndex: "1493495687878955008",
+  isRt: true,
+  name: "woo!ah!_JP",
+  screen_name: "wooah_jp",
+  full_text:
+    "[#wa_video🎥]\n#ソラの突撃リポート🎤EP.18\n「#wooahに質問」にメンバーが答えます😍📝\n今日は #ルーシー です💙\nルーシーのwow!への熱い気持ちが…🥲✨\nぜひチェックしてください😉🎶\n\n#wooah #우아 #소라 #ソラ #루시\n#wooah_TMI https://t.co/17nMZoX7x4",
+  created_at: "Tue Feb 15 04:20:00 +0000 2022",
+  hashtags: [
+    "wa_video",
+    "ソラの突撃リポート",
+    "wooahに質問",
+    "ルーシー",
+    "wooah",
+    "우아",
+    "소라",
+    "ソラ",
+    "루시",
+    "wooah_TMI",
+  ],
+  urls: [],
+  media: [
+    {
+      type: "video",
+      url: "https://t.co/17nMZoX7x4",
+      src: "https://video.twimg.com/ext_tw_video/1493439313496793089/pu/vid/1280x720/mFWuIXA3DHwcU3ze.mp4?tag=12",
+      poster:
+        "https://pbs.twimg.com/ext_tw_video_thumb/1493439313496793089/pu/img/3YCxrHmunJSBP4RJ.jpg",
+    },
+  ],
+  meta: [],
+};
+export default dataMedia as Twitter;
