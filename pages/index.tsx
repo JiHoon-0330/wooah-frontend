d106c5319e81a42e2c938cd907c6f636cabb52c8
jihoon0330 <jihoon@hoztec.com>
2022-03-06 23:16:55 +0900

    ssr 주석

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,28 @@
-import { dehydrate, QueryClient } from "react-query";
 import Weverse from "../components/Templates/Weverse/Weverse";
-import apiAxios from "../services/api/axios";
+// import { dehydrate, QueryClient } from "react-query";
+// import apiAxios from "../services/api/axios";
 
 const HomePage = () => {
   return <Weverse />;
 };
 
-export async function getServerSideProps() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchInfiniteQuery(
-    "GET WEVERSE_POST /weverse",
-    async () => {
-      const { data } = await apiAxios({
-        url: "/weverse",
-      });
-      return data;
-    },
-  );
+// export async function getServerSideProps() {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchInfiniteQuery(
+//     "GET WEVERSE_POST /weverse",
+//     async () => {
+//       const { data } = await apiAxios({
+//         url: "/weverse",
+//       });
+//       return data;
+//     },
+//   );
 
-  return {
-    props: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-    },
-  };
-}
+//   return {
+//     props: {
+//       dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+//     },
+//   };
+// }
 
 export default HomePage;
--- pages/twitter.tsx
+++ pages/twitter.tsx
@@ -1,28 +1,28 @@
-import { dehydrate, QueryClient } from "react-query";
 import Twitter from "../components/Templates/Twitter/Twitter";
-import apiAxios from "../services/api/axios";
+// import { dehydrate, QueryClient } from "react-query";
+// import apiAxios from "../services/api/axios";
 
 const TwitterPage = () => {
   return <Twitter />;
 };
 
-export async function getServerSideProps() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchInfiniteQuery(
-    "GET TWITTER_POST /twitter",
-    async () => {
-      const { data } = await apiAxios({
-        url: "/twitter",
-      });
-      return data;
-    },
-  );
+// export async function getServerSideProps() {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchInfiniteQuery(
+//     "GET TWITTER_POST /twitter",
+//     async () => {
+//       const { data } = await apiAxios({
+//         url: "/twitter",
+//       });
+//       return data;
+//     },
+//   );
 
-  return {
-    props: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-    },
-  };
-}
+//   return {
+//     props: {
+//       dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+//     },
+//   };
+// }
 
 export default TwitterPage;
